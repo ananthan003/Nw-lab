ea7a406067159f6a884252c135e0e3a46d00d10b
ananthan003 <ananthu.ananthan003@gmail.com>
2025-03-04 20:01:01 +0530

    Add files

---
+++ prgm18.sh
@@ -0,0 +1,22 @@
+echo "Enter the filename:"
+read filename
+if [ -e "$filename" ]; then
+echo "file exists"
+if [ -r "$filename" ]; then
+echo "file is readable"
+else
+echo "file is not readable"
+fi
+if [ -w "$filename" ]; then
+echo "file is writable"
+else
+echo "file is not writable"
+fi
+if [ -r "$filename" ] && [ -w "$filename" ]; then
+echo "file is both readable and writable"
+else
+echo "file is not both readable and writable"
+fi
+else
+echo "file dont exists"
+fi
